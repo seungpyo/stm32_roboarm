c664ec1c5f939be2d7bde692296ae9fe08dffc85
2017030500 <2017030500@hanyang.ac.kr>
2019-08-03 19:11:08 +0900

    Added some features for usart prints

--- usart/main.c
+++ usart/main.c
@@ -1,37 +1,45 @@
 #include "regs.h"
 #include "led.h"
+#define USART_FREQ (4*1000000)
+#define USART_BAUD (9600)
 
-int puts(const char *str)
-{
 
-	// Uses PCLK as a default clock.
-	led_4states(1);
-	while (*str) {
-
-		int bark = 0;
-		while (!(*(USART2_ISR) & USART_ISR_TXE_MASK)) {
-			// Oscillating LEDs for an infiite loop.
-			led_blink(bark++&1?8:6, 12);
-		}
-		led_4states(2);		
-		*(USART2_TDR) = *str++ & 0xFF;
-
-	}
-	led_4states(3);
-	return 0;
+/**
+  * puts a character c through USART2.
+  * Polls TXE bit in USART_ISR
+  */
+void usart_putc(const char c) {
+	while (!(*(USART2_ISR) & USART_ISR_TXE_MASK));
+	*(USART2_TDR) = (c & 0xFF);
 }
 
+/**
+  * transmits a string through USART2.
+  * returns a number of bytes printed.
+  */
+int usart_puts(const char *str) {
+	int ret = 0;
+	while (*str) {
+		usart_putc(*str);
+		str++;
+		ret++;
+	}
+	return ret;
+}
 
-int main (void) {
-	
-	led_init();
-	// Enable Clocks
+void usart_println(const char *str) {
+	usart_puts(str);
+	usart_putc('\r');
+	usart_putc('\n');
+}
+
+void usart_init() {
+
+	// Enable USART2, GPIOA clock
 	*(RCC_APB1ENR1) |= RCC_USART2EN_MASK;
-	// I hate writing at GPIOA.
 	*(RCC_AHB2ENR) |= RCC_GPIOAEN_MASK;
-	// *(RCC_AHB2ENR) |= RCC_GPIODEN_MASK;
 
-	// Set up GPIOA alternate function
+	// Set up GPIOA alternate functions (Tx, Rx)
 	*(GPIOA_AFRL) = GPIOA_AF_USART2_TX_MASK;
 	*(GPIOA_MODER) &= ~GPIO_PIN_MASK(2, GPIO_MODE_RESET);
 	*(GPIOA_MODER) |= GPIO_PIN_MASK(2, GPIO_MODE_AF);
@@ -40,21 +48,33 @@ int main (void) {
 	*(GPIOA_MODER) &= ~GPIO_PIN_MASK(3, GPIO_MODE_RESET);
 	*(GPIOA_MODER) |= GPIO_PIN_MASK(3, GPIO_MODE_AF);
 
-	// Character transmission protocol,
-	// based on RM0351 page 1340.
+	// Turn off USART to set baud rate
 	*(USART2_CR1) &= ~USART_CR1_UE_MASK;
 	
-#define FREQ (4*1000000)
-#define BAUD (9600)
-	// The setting above yields error of 0.16%.
-	uint16_t usartdiv = (float)FREQ / BAUD;
+	// The setting below yields error of 0.16%, 
+	// which isn't that problematic.
+	uint16_t usartdiv = (float)USART_FREQ / USART_BAUD;
 	
+	// Set USARTDIV as (freq = 4MHz / baud = 9600)
 	*(USART2_BRR) = usartdiv;
 
+	// Enable Rx, Tx in USART
 	*(USART2_CR1) |= USART_CR1_RE_MASK;
 	*(USART2_CR1) |= USART_CR1_TE_MASK;
+	
+	// For now, we don't use TXEIE
 	// *(USART2_CR1) |= USART_CR1_TXEIE_MASK;
+
+	// Now, turn on USART
 	*(USART2_CR1) |= USART_CR1_UE_MASK;
 
-	puts("Hello World!");
+}
+
+int main (void) {
+	
+	led_init();
+	usart_init();
+	
+	usart_puts("Hello World!\r\n");
+	usart_println("Nice to meet you!");
 }
